544e9e1a4a98481df0382ff64334372709931e66
Dorsainvil <l.dorsainvil397@mtlocal.lan>
2024-05-23 12:51:36 -0400

    Began making function for play buttton

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var themeToggle = document.querySelector('.themeToggle');
+var playBtn = document.getElementById('playPause');
+var isPlaying = false;
 
 themeToggle.addEventListener('click', function() {
     document.body.classList.toggle('darkBody');
@@ -8,4 +10,16 @@ themeToggle.addEventListener('click', function() {
    else {
     themeToggle.setAttribute("name", "moon");
    }
+})
+
+playBtn.addEventListener('click', function() {
+
+    if (!isPlaying) {
+        isPlaying = true;
+        playBtn.setAttribut("name", "play");
+    }
+   else if (isPlaying) {
+        isPlaying = false;
+        playBtn.setAttribute("name", "pause");
+    }
 })
