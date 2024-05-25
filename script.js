02b90dd2ed1de95f34ac3b3d17595607c3e695c5
Lens <lensdorsainvil1@gmail.com>
2024-05-25 14:57:19 -0400

    Added audio and made progress bar as well as audio controls work

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var themeToggle = document.querySelector('.themeToggle');
 var playBtn = document.getElementById('playPause');
+var skipForward = document.getElementById('playForward');
+var skipBack = document.getElementById('playBack');
+var progressBar = document.querySelector('.progressBar');
 let isPlaying = false;
+var music = new Audio("once-in-paris-168895.mp3");
 
 themeToggle.addEventListener('click', function() {
     document.body.classList.toggle('darkBody');
@@ -12,18 +16,36 @@ themeToggle.addEventListener('click', function() {
    }
 })
 
+function moveProgress() {
+    progressBar.style.width = `${music.currentTime}%`
+}
+
+setInterval(moveProgress, 1000);
+
 playBtn.addEventListener('click', function() {
 
     if (!isPlaying) {
         isPlaying = true;
         playBtn.setAttribute("name", "play");
         console.log("not playing");
+        music.pause()
         return;
     }
     if (isPlaying) {
          isPlaying = false;
          playBtn.setAttribute("name", "pause");
-         console.log("playing!")
+         console.log("playing!");
+         music.play();
          return;
      }
 })
+
+skipForward.addEventListener('click', function() {
+    music.currentTime = music.currentTime + 25;
+    moveProgress();
+});
+
+skipBack.addEventListener('click', function() {
+    music.currentTime = music.currentTime - 25;
+    moveProgress();
+});
